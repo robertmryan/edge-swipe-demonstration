9c49645e75b1b4ebe40940b0149ce59c8c65d155
Rob Ryan <robert.ryan@mindspring.com>
2013-07-09 10:42:53 -0400

    Initial Commit

---
+++ Edge Swipe Demonstration/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Edge Swipe Demonstration
+//
+//  Created by Robert Ryan on 7/9/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
